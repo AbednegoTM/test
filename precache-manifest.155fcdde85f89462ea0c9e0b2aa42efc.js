f3044bd1ab1235d86c5e26608a815af872da37a9
abednegotm <abednegomwanza@gmail.com>
2020-06-03 22:17:01 +0200

    Updates

---
+++ precache-manifest.155fcdde85f89462ea0c9e0b2aa42efc.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ed95847899a7a28bdcc2c16cc7ad8aa",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "e4dbe440aeaafa0ca1b4",
+    "url": "/test/static/css/main.4eca2ba1.chunk.css"
+  },
+  {
+    "revision": "e71c821cca0edd47e322",
+    "url": "/test/static/js/2.56f878d4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/test/static/js/2.56f878d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4dbe440aeaafa0ca1b4",
+    "url": "/test/static/js/main.eb3226f0.chunk.js"
+  },
+  {
+    "revision": "84e53d695275b8b422d0",
+    "url": "/test/static/js/runtime-main.d4876194.js"
+  },
+  {
+    "revision": "079052da4c90a5561ac2ffe818012b66",
+    "url": "/test/static/media/Capture.079052da.png"
+  },
+  {
+    "revision": "5b079e57406179f8d27106ddde2d9cb7",
+    "url": "/test/static/media/Down_by_the_River.5b079e57.mp4"
+  },
+  {
+    "revision": "a032f73fe01bbc49c8fc8ebdd4671b85",
+    "url": "/test/static/media/Energy.a032f73f.png"
+  },
+  {
+    "revision": "0cc77962739bf61d67513681c58b4e19",
+    "url": "/test/static/media/Gellix-Regular.0cc77962.ttf"
+  },
+  {
+    "revision": "2f15a9f37e3235a0bd5aa54135243f61",
+    "url": "/test/static/media/Gellix-Regular.2f15a9f3.woff2"
+  },
+  {
+    "revision": "6645d1b8877432cc38934b47dc7b4f03",
+    "url": "/test/static/media/Gellix-Regular.6645d1b8.woff"
+  },
+  {
+    "revision": "c4143c829a3ab56770de0b39c343aed9",
+    "url": "/test/static/media/Le1.c4143c82.png"
+  },
+  {
+    "revision": "9417448617fe40cd8beaabc092773b6d",
+    "url": "/test/static/media/Learning.94174486.png"
+  },
+  {
+    "revision": "43e27845779ccb66c50d19dd98a786bc",
+    "url": "/test/static/media/bcv.43e27845.png"
+  },
+  {
+    "revision": "219c693fe1b0471c0d329d8a74db0ccc",
+    "url": "/test/static/media/eps.219c693f.png"
+  },
+  {
+    "revision": "5cfeecdb437e345fa7b05f03e06556f6",
+    "url": "/test/static/media/fik.5cfeecdb.png"
+  },
+  {
+    "revision": "d5a946805ef61281720393bf51c9a45a",
+    "url": "/test/static/media/map.d5a94680.png"
+  }
+]);
